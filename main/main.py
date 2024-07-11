a02cfd0c6d242f11a190d46b69b5d9dc2a6c6a21
Aleksei Moskaljuk <alekseiko@gmail.com>
2024-07-11 14:39:28 +0200

    Add logger

--- main/main.py
+++ main/main.py
@@ -10,20 +10,28 @@ from urllib.parse import urlparse, parse_qs, urlencode
 import pandas as pd
 from openpyxl.workbook import Workbook
 import streamlit as st
+import logging
+# from dotenv import load_dotenv
+
+# Load environment variables
+# load_dotenv()
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger('myLogger')
 
 # Constants
 URL = "https://www.azair.eu/azfin.php"
 # TODO: make token an environment variable
 # BOT_TOKEN = os.environ.get('BOT_TOKEN')
+BOT_TOKEN = "6824381438:AAFaoV_9QF8trD56obqJL5BsPSDY3xMG_8o"
 CHAT_ID = 324109540
 YEAR = 2024
 
 # Initialize bot
-BOT = telebot.TeleBot("6824381438:AAFaoV_9QF8trD56obqJL5BsPSDY3xMG_8o")
+BOT = telebot.TeleBot(BOT_TOKEN)
 
 def get_distinct_months(current_date=date.today(), num_months=12):
     distinct_months = set()  # Using a set to store unique month+YEAR combinations
-    
     current_month = current_date.month  # Get the current month
     
     for _ in range(num_months):
@@ -42,7 +50,13 @@ def get_distinct_months(current_date=date.today(), num_months=12):
     # Convert the set to a list and sort it to ensure the months are in ascending order
     return sorted(list(distinct_months))
 
+
 def get_weekends(month=date.today().month):
+    """
+    Get weekends for a given month.
+    :param month: Month for which to get weekends.
+    :return: List of tuples with start and end dates for weekends.
+    """
     weekends = []
     c = calendar.TextCalendar(calendar.MONDAY)
     
@@ -58,21 +72,21 @@ def get_weekends(month=date.today().month):
                 elif day.weekday() == 0: # Monday
                     start_date = start_day.strftime("%Y%m%d")
                     end_date = day.strftime("%Y%m%d")
-                    
                     weekends.append((start_date, end_date))
-                    
                     start_day = None
                 
     return weekends           
 
 def scrape_flight_details(url):
-    response = requests.get(url)
-    if response.status_code == 200: # OK
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # Raise an HTTPError for bad responses (4xx and 5xx)
         soup = BeautifulSoup(response.content, 'html.parser')
         flight_details = soup.find_all('div', class_='result')
         return flight_details
-    else:
-        print("Error:", response.status_code)
+    except requests.exceptions.RequestException as e:
+        logger.error(e)
+        print("Error scraping {url}: {e}")
         return None
     
 
@@ -80,17 +94,42 @@ def parse_flight_details(soup):
     flight_details = soup.find_all('div', class_='result')
     return flight_details
 
-def send_notification(offer):
-    # Implement your notification logic here
-    pass
-
 # TODO: compare parsed price from Azair with the price obtained through Ryanair API (if it's Ryanair airline)
-def get_leg_price_from_ryanair(price):
+def get_leg_price_from_ryanair_api(price):
     pass
 
 # TODO: check the price prediction on airhint 
 def buy_or_wait(offer):
     pass
+
+def update_params(params, depdate, arrdate):
+    params["depdate"] = depdate
+    params["arrdate"] = arrdate
+    return URL + "?" + urlencode(params)
+
+def process_weekends(weekends, original_params):
+    results = []
+    for date_range in weekends:
+        updated_url = update_params(original_params.copy(), date_range[0], date_range[1])
+        flight_details = scrape_flight_details(updated_url)
+        if (flight_details):
+            for detail in flight_details:
+                offer = offer_parser.parse_offer(detail)
+                if offer:
+                    results.append({
+                        'Departure Date': offer.outbound_date,
+                        'Outbound Departure Time': offer.outbound_departure_time,
+                        'Arrival Date': offer.inbound_date,
+                        'Outbound airline:': offer.outbound_airline,
+                        'Inbound Departure Time': offer.inbound_departure_time,
+                        'Origin': offer.origin_airport,
+                        'Destination': offer.destination_airport,
+                        'Inbound airline:': offer.inbound_airline,
+                        'Total Price': offer.total_price
+                    })
+    
+    return results
+                        
 
 # entry point
 def main():    
@@ -147,26 +186,8 @@ def main():
     print(weekends)
     
     # Create a list to store the results
-    results = []
+    results = process_weekends(weekends, original_params)
     
-    for date_range in weekends:
-            # Update departure and arrival dates in query parameters
-            params = original_params.copy()
-            params["depdate"] = date_range[0]
-            params["arrdate"] = date_range[1]
-            
-            # Construct URL with updated parameters
-            updated_url = URL + "?" + urlencode(params)
-            
-            # Scrape result page
-            flight_details = scrape_flight_details(updated_url)
-            
-            if flight_details:
-                for detail in flight_details:
-                    offer = offer_parser.parse_offer(detail)
-                    if offer is not None:
-                        results.append({'Departure Date' : offer.outbound_date, 'Outbound Departure Time': offer.outbound_departure_time, 'Arrival Date' : offer.inbound_date, 'Outbound airline:' : offer.outbound_airline, 'Inbound Departure Time': offer.inbound_departure_time, 'Origin': offer.origin_airport, 'Destination': offer.destination_airport, 'Inbound airline:': offer.inbound_airline, 'Total Price': offer.total_price})
-                
     # Convert results list to DataFrame
     results_df = pd.DataFrame(results)    
 
@@ -179,7 +200,7 @@ def main():
     results_df
     
     # Indicate that the file has been created
-    print(f"File '{file_path}' has been created successfully.")
+    logger.info(f"File '{file_path}' has been created successfully.")
 
 if __name__ == "__main__":
     main()
